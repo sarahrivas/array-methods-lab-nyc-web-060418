20845ccd59f947e100354697b6078ec1a4c533bd
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-26 23:50:37 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,20 +4,21 @@ end
 
 def using_sort(array)
   sorted_array = array.sort
+  sorted_array
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
 def using_first(array)
-
+  array.first
 end
 
 def using_last(array)
-
+  array.last
 end
 
 def using_size(array)
-
+  array.size
 end
